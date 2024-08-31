8b94c07b15fb7ee8209f6682e7425cd41816de53
Justin Grant Gugol <gugol.184156@globalcity.sti.edu.ph>
2024-09-01 03:13:19 +0800

    konting change lng

--- DO DESK/JAVASCRIPT/populateName.js
+++ DO DESK/JAVASCRIPT/populateName.js
@@ -2,7 +2,20 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('../PHP/getUserData.php')
         .then(response => response.json())
         .then(userData => {
-            document.getElementById('accountName').textContent = `${userData.firstName} ${userData.middleName} ${userData.lastName}`;
+            if (userData.middleName != null) {
+                if (userData.suffixName  != null) {
+                    document.getElementById('accountName').textContent = `${userData.firstName} ${userData.middleName} ${userData.lastName} ${userData.suffixName}`;
+                } else {
+                    document.getElementById('accountName').textContent = `${userData.firstName} ${userData.middleName} ${userData.lastName}`;
+                }
+            } else {
+                if (userData.suffixName != null) {
+                    document.getElementById('accountName').textContent = `${userData.firstName} ${userData.lastName} ${userData.suffixName}`;
+                } else {
+                    document.getElementById('accountName').textContent = `${userData.firstName} ${userData.lastName}`;
+                }
+            }
+            
             document.getElementById('accountRole').textContent = userData.role;
         })
         .catch(error => console.error('Error fetching user data:', error));
--- DO DESK/PHP/getUserData.php
+++ DO DESK/PHP/getUserData.php
@@ -5,6 +5,7 @@ echo json_encode([
     'firstName' => $_SESSION['firstName'],
     'middleName' => $_SESSION['middleName'],
     'lastName' => $_SESSION['lastName'],
+    'suffixName' => $_SESSION['suffixName']
     'role' => $_SESSION['role']
 ]);
 ?>
