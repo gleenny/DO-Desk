7edb2ba71c12ec947479b722df41c2935a7e5c95
Gleenn <gleenn2002@gmail.com>
2024-10-16 02:05:26 +0800

    ayos na ung recordlist sa dashboard ayos na ung record page may mga modal na sya

--- DO DESK/JAVASCRIPT/DODesk-RecordsJS.js
+++ DO DESK/JAVASCRIPT/DODesk-RecordsJS.js
@@ -6,6 +6,47 @@ const updateStatusForm = document.querySelector('#updateStatusForm');
 let rowCount = 0;
 getViolationInfo();
 
+// Get the modal
+var modal1 = document.getElementById("modalSubmit");
+var modal2 = document.getElementById("modalUpdate");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("btnSubmit");
+var btn2 = document.getElementById("btnUpdate");
+
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on the button, open the modal
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+btn2.onclick = function() {
+    modal2.style.display = "block";
+  }
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+span2.onclick = function() {
+    modal2.style.display = "none";
+  }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+  else if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
+
 //adding new violations
 form.addEventListener('submit', (e) => {
     e.preventDefault()
